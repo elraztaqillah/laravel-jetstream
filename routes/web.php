ecf23e814ffd1656a5e1a967cc5363d564bb04f2
Elraz <elraztaqillah@gmail.com>
2025-12-26 11:29:43 +0700

    update

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Http\Controllers\TodoController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+
+Route::resource('todo', TodoController::class)->middleware('auth:sanctum');
+    // harus login agar bisa akses halaman 
